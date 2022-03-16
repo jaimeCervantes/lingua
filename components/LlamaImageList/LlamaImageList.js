3ffe3b8769b1cac936967acae29d5a2059957227
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-03-16 15:22:40 -0600

    refactor: delete info icon form list of images

--- components/LlamaImageList/LlamaImageList.js
+++ components/LlamaImageList/LlamaImageList.js
@@ -1,8 +1,6 @@
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
@@ -29,14 +27,6 @@ export default function LlamaImageList({ images }) {
             <ImageListItemBar
               title={item.title}
               subtitle={item.author}
-              actionIcon={
-                <IconButton
-                  sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                  aria-label={`info about ${item.title}`}
-                >
-                  <InfoIcon />
-                </IconButton>
-              }
             />
         </ImageListItem>
       ))}
