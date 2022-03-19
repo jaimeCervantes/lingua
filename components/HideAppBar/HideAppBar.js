e89a7de3e53eab75e9a90d5cebf8a7ea89019c04
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-03-19 16:26:53 -0600

    feat: add three app bar with logo and social icons

--- components/HideAppBar/HideAppBar.js
+++ components/HideAppBar/HideAppBar.js
@@ -1,11 +1,20 @@
-import { AppBar, Toolbar, Typography, Slide, Box, Button } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Slide, Box, Button } from '@mui/material';
 import useScrollTrigger from "@mui/material/useScrollTrigger";
+import { 
+  Facebook as FacebookIcon,
+  YouTube as YouTubeIcon,
+  Instagram as InstagramIcon,
+  WhatsApp as WhatsAppIcon,
+} from '@mui/icons-material';
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
 
-function HideOnScroll({ children }) {
+import DiscordIcon from '../Icons/DiscordIcon';
+
+function ShowOnScroll({ children }) {
   const trigger = useScrollTrigger();
 
   return (
-    <Slide appear={false} direction="down" in={!trigger}>
+    <Slide appear={false} direction="down" in={trigger}>
       {children}
     </Slide>
   );
@@ -14,15 +23,79 @@ function HideOnScroll({ children }) {
 export default function HideAppBar(props) {
   return (
     <>
-      <HideOnScroll {...props}>
-        <AppBar sx={{ backgroundColor: 'rgb(85, 0, 0)'}}>
+
+        <AppBar position="static" sx={{ backgroundColor: 'rgb(85, 0, 0)' }} elevation="0">
           <Toolbar disableGutters sx={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
-            <img src="/logo-white.svg" height="40" />
-            <Box sx={{ flexGrow: 1, display: 'flex', paddingLeft: '1rem' }}>
+          <div style={{ flexGrow: 1 }}>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="Facebook"
+              href="https://www.facebook.com/LinguaLlama2021/"
+              target="_blank"
+              >
+              <FacebookIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="Instagram"
+              href="https://www.instagram.com/linguallama/"
+              target="_blank"
+            >
+              <InstagramIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="WhatsApp"
+              href="https://wa.me/12018380698"
+              target="_blank"
+            >
+              <WhatsAppIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="Discord"
+              href="https://disboard.org/server/762367346890768394"
+              target="_blank"
+            >
+              <DiscordIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="YouTube"
+              href="https://www.youtube.com/c/LINGUALLAMAServingAllLanguagesWorldwide"
+              target="_blank"
+            >
+              <YouTubeIcon />
+            </IconButton>
+            </div>
+            <Button variant="text" sx={{ color: 'white', fontSize: '1.4rem' }} href="tel:+12018380698">
+              +1 201 838 0698 <PhoneInTalkIcon sx={{ marginLeft: '0.5rem' }}/>
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+
+        <AppBar position="relative" color="secondary" elevation="0">
+          <Toolbar disableGutters sx={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
+            <img
+              src="/logo-white.svg" height="100"
+              style={{
+                marginRight: '1rem',
+                paddingTop: '0.7rem',
+                paddingBottom: '0.7rem'
+              }}
+            ></img>
+            <Box sx={{ flexGrow: 1, display: { sm: 'flex', xs: 'none' } }}>
               {['Home', 'Events', 'About'].map((page) => (
                 <Button
                   key={page}
-                  sx={{ color: 'white', display: 'inline-block' }}
+                  sx={{
+                    color: 'white',
+                    display: 'inline-block',
+                    '&:hover': {
+                      textDecoration: 'underline'
+                    }
+                  }}
                 >
                   {page}
                 </Button>
@@ -30,9 +103,78 @@ export default function HideAppBar(props) {
             </Box>
           </Toolbar>
         </AppBar>
-      </HideOnScroll>
 
-      <Toolbar />
+
+      <ShowOnScroll {...props}>
+        <AppBar color="secondary">
+          <Toolbar disableGutters sx={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
+            <img
+              src="/logo-white.svg" height="90"
+              style={{
+                marginRight: '1rem',
+                paddingTop: '0.7rem',
+                paddingBottom: '0.7rem'
+              }}
+            ></img>
+            <Box sx={{ flexGrow: 1, display: { sm: 'flex', xs: 'none' } }}>
+              {['Home', 'Events', 'About'].map((page) => (
+                <Button
+                  key={page}
+                  sx={{
+                    color: 'white',
+                    display: 'inline-block',
+                    '&:hover': {
+                      textDecoration: 'underline'
+                    }
+                  }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="Facebook"
+              href="https://www.facebook.com/LinguaLlama2021/"
+              target="_blank"
+              >
+              <FacebookIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="Instagram"
+              href="https://www.instagram.com/linguallama/"
+              target="_blank"
+            >
+              <InstagramIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="WhatsApp"
+              href="https://wa.me/12018380698"
+              target="_blank"
+            >
+              <WhatsAppIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="Discord"
+              href="https://disboard.org/server/762367346890768394"
+              target="_blank"
+            >
+              <DiscordIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: 'white' }}
+              aria-label="YouTube"
+              href="https://www.youtube.com/c/LINGUALLAMAServingAllLanguagesWorldwide"
+              target="_blank"
+            >
+              <YouTubeIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </ShowOnScroll>
     </>
   );
 }
