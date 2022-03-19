a3ed93aa6d033c60ce4c656ee7149acbde87dda6
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-03-18 19:52:46 -0600

    feat: create basict structure for home page and create scroll hiding appbar

---
+++ components/HideAppBar/HideAppBar.js
@@ -0,0 +1,38 @@
+import { AppBar, Toolbar, Typography, Slide, Box, Button } from '@mui/material';
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+
+function HideOnScroll({ children }) {
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+export default function HideAppBar(props) {
+  return (
+    <>
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar disableGutters sx={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
+            <img src="/logo-white.svg" height="40" />
+            <Box sx={{ flexGrow: 1, display: 'flex', paddingLeft: '1rem' }}>
+              {['Home', 'Events', 'About'].map((page) => (
+                <Button
+                  key={page}
+                  sx={{ color: 'white', display: 'inline-block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+
+      <Toolbar />
+    </>
+  );
+}
