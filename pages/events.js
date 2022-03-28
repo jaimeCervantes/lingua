33261e8bf1699834b927e8ffe8ed3839f6ce4877
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-03-27 19:10:41 -0600

    refactor: don't pass time zone to embedded url for calendars

--- pages/events.js
+++ pages/events.js
@@ -19,17 +19,17 @@ export default function Events({ events, languages }) {
 
         <LlamaEmbeddedCalendar
           sx={{ marginBottom: "1rem" }}
-          src="https://calendar.google.com/calendar/embed?src=p4hbngft52jpv1jgojuoi5qjm4%40group.calendar.google.com&ctz=America%2FMexico_City"
+          src="https://calendar.google.com/calendar/embed?src=p4hbngft52jpv1jgojuoi5qjm4%40group.calendar.google.com"
         />
 
         <LlamaEmbeddedCalendar
           sx={{ marginBottom: "1rem" }}
-          src="https://calendar.google.com/calendar/embed?src=d75eds8vnivm5cvoeg2r913l18%40group.calendar.google.com&ctz=America%2FMexico_City"
+          src="https://calendar.google.com/calendar/embed?src=d75eds8vnivm5cvoeg2r913l18%40group.calendar.google.com"
         />
 
         <LlamaEmbeddedCalendar
           sx={{ marginBottom: "1rem" }}
-          src="https://calendar.google.com/calendar/embed?src=lquoh06ikbd3j0cnus0fa4gn64%40group.calendar.google.com&ctz=America%2FMexico_City"
+          src="https://calendar.google.com/calendar/embed?src=lquoh06ikbd3j0cnus0fa4gn64%40group.calendar.google.com"
         />
       </Box>
 
