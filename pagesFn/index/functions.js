6998e23f9ccac60cf4de90035422ee0f215f339c
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-04-26 07:28:14 -0500

    refactor: use getServerSideProps for index page

--- pagesFn/index/functions.js
+++ pagesFn/index/functions.js
@@ -13,7 +13,7 @@ export function getLayout(page) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(ctx) {
   const [images, tours, index, languages ] = await Promise.all([
     fetchAPI('/home-images/', {
       populate: {
