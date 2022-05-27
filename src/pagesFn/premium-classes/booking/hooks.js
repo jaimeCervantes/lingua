e5d050a137a4299dd4ecfac9af3b4970ca0da69a
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-05-27 15:12:57 -0500

    refactor: change booking hooks and retrive only producs with metadad type equals "class"

--- src/pagesFn/premium-classes/booking/hooks.js
+++ src/pagesFn/premium-classes/booking/hooks.js
@@ -1,21 +1,14 @@
 import { useState, useEffect } from 'react';
 import { fetchSchedules } from './functions';
-import {
-  mapWeekRecurringEventsToEvents,
-  matchSchedulesWithEvents,
-  mapSchedules
-} from './mappers';
 
-export function useMatchedSchedules(id, recurringEvents, isRequestingSchedules, fromDate) {
+export function useMatchedSchedules(id, mappedEvents, isRequestingSchedules, mapper) {
   const [matches, setMatches] = useState([])
 
   useEffect(() => {
     if (isRequestingSchedules) {
-      const mappedEvents = mapWeekRecurringEventsToEvents(fromDate, recurringEvents);
-      
       (async () => {
         const response = await fetchSchedules(mappedEvents, id);
-        const matched = matchSchedulesWithEvents(mapSchedules(response.data), mappedEvents, id);
+        const matched = mapper(response.data, mappedEvents, id);
         
         setMatches(matched);
       })();
