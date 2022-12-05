0fc7e20a98f3cdabf4bfd42ce7aa730cb43a97af
Jaime Cervantes <jaime.cervantes.ve@gmail.com>
2022-12-05 15:08:21 -0600

    refactor: create custom hook to controll widget insertion

---
+++ components/pages/index/useLinguaHouse.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+
+export default function useLinguaHouseWidget(container) {
+  useEffect(() => {
+    let iframe = null;
+    if (container?.current) {
+      let url = "https://www.linguahouse.com/remote/profile/2a623461-65ed-5b1e-10f7-ffb9724618ed";
+      const newPasswordUrl = "https://www.linguahouse.com/remote/new-password/2a623461-65ed-5b1e-10f7-ffb9724618ed";
+      const verifyEmailUrl = "https://www.linguahouse.com/remote/verify-email/2a623461-65ed-5b1e-10f7-ffb9724618ed";
+      const addPdfCodeUrl = "https://www.linguahouse.com/remote/a/2a623461-65ed-5b1e-10f7-ffb9724618ed";
+      const query = parseQuery(window.location.search);
+      const width = "100%";
+      const height = "1000";
+      
+      iframe = document.createElement('iframe');
+      
+      if (query.new_password_token) {
+          url = newPasswordUrl + '/' + query.new_password_token;
+      } else if (query.verify_email_token) {
+          url = verifyEmailUrl + '/' + query.verify_email_token;
+      } else if (query.add_pdf_code) {
+          url = addPdfCodeUrl + '/' + query.add_pdf_code;
+      }
+      
+      iframe.setAttribute('class', 'linguahouse_profile_widget');
+      iframe.setAttribute('src', url);
+      iframe.setAttribute('width', width);
+      iframe.setAttribute('height', height);
+      iframe.setAttribute('frameborder', '0');
+      container.current.appendChild(iframe);
+    }
+  
+    return () => {
+      iframe?.remove();
+    }
+  }, [container])
+}
+
+function parseQuery(e){for(var n={},o=("?"===e[0]?e.substr(1):e).split("&"),r=0;r<o.length;r++){var t=o[r].split("=");n[decodeURIComponent(t[0])]=decodeURIComponent(t[1]||"")}return n}
--- pages/index.js
+++ pages/index.js
@@ -8,48 +8,14 @@ import LlamaMenu from "../components/LlamaMenu/LlamaMenu";
 import LlamaDialog from "../components/LlamaDialog/LlamaDialog";
 import { DialogContent } from "@mui/material";
 import LlamaPlacementTestButton from "../components/Buttons/LlamaPlacementTestButton";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
+import useLinguaHouseWidget from "../components/pages/index/useLinguaHouse";
 
 export { getStaticProps } from "../pagesFn/index/functions";
-
-function parseQuery(e){for(var n={},o=("?"===e[0]?e.substr(1):e).split("&"),r=0;r<o.length;r++){var t=o[r].split("=");n[decodeURIComponent(t[0])]=decodeURIComponent(t[1]||"")}return n}
-
 export default function Index({ homeImages, index, languages }) {
   const linguahouseContainer = useRef(null);
   
-  useEffect(() => {
-    let iframe = null;
-    if (linguahouseContainer?.current) {
-      let url = "https://www.linguahouse.com/remote/profile/2a623461-65ed-5b1e-10f7-ffb9724618ed";
-      const newPasswordUrl = "https://www.linguahouse.com/remote/new-password/2a623461-65ed-5b1e-10f7-ffb9724618ed";
-      const verifyEmailUrl = "https://www.linguahouse.com/remote/verify-email/2a623461-65ed-5b1e-10f7-ffb9724618ed";
-      const addPdfCodeUrl = "https://www.linguahouse.com/remote/a/2a623461-65ed-5b1e-10f7-ffb9724618ed";
-      const query = parseQuery(window.location.search);
-      const width = "100%";
-      const height = "800";
-      
-      iframe = document.createElement('iframe');
-      
-      if (query.new_password_token) {
-          url = newPasswordUrl + '/' + query.new_password_token;
-      } else if (query.verify_email_token) {
-          url = verifyEmailUrl + '/' + query.verify_email_token;
-      } else if (query.add_pdf_code) {
-          url = addPdfCodeUrl + '/' + query.add_pdf_code;
-      }
-      
-      iframe.setAttribute('class', 'linguahouse_profile_widget');
-      iframe.setAttribute('src', url);
-      iframe.setAttribute('width', width);
-      iframe.setAttribute('height', height);
-      iframe.setAttribute('frameborder', '0');
-      linguahouseContainer.current.appendChild(iframe);
-    }
-
-    return () => {
-      iframe?.remove();
-    }
-  }, [linguahouseContainer])
+  useLinguaHouseWidget(linguahouseContainer)
 
   return (
     <>
