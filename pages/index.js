1a5efd4d08b5bbe65c7c8da842fd444b20ff0719
Jaime Cervantes <jaime.cervantes.ve@gmail.com>
2022-12-05 14:45:03 -0600

    feat: test linguahouse widget

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,42 @@ import { useEffect, useRef } from "react";
 
 export { getStaticProps } from "../pagesFn/index/functions";
 
+function parseQuery(e){for(var n={},o=("?"===e[0]?e.substr(1):e).split("&"),r=0;r<o.length;r++){var t=o[r].split("=");n[decodeURIComponent(t[0])]=decodeURIComponent(t[1]||"")}return n}
+
 export default function Index({ homeImages, index, languages }) {
   const linguahouseContainer = useRef(null);
   
   useEffect(() => {
-    let elem = null;
+    let iframe = null;
     if (linguahouseContainer?.current) {
-      elem = document.createElement('script');
-      elem.src = 'https://www.linguahouse.com/widgets/profile/2a623461-65ed-5b1e-10f7-ffb9724618ed';
-      linguahouseContainer.current.appendChild(elem);
+      let url = "https://www.linguahouse.com/remote/profile/2a623461-65ed-5b1e-10f7-ffb9724618ed";
+      const newPasswordUrl = "https://www.linguahouse.com/remote/new-password/2a623461-65ed-5b1e-10f7-ffb9724618ed";
+      const verifyEmailUrl = "https://www.linguahouse.com/remote/verify-email/2a623461-65ed-5b1e-10f7-ffb9724618ed";
+      const addPdfCodeUrl = "https://www.linguahouse.com/remote/a/2a623461-65ed-5b1e-10f7-ffb9724618ed";
+      const query = parseQuery(window.location.search);
+      const width = "100%";
+      const height = "800";
+      
+      iframe = document.createElement('iframe');
+      
+      if (query.new_password_token) {
+          url = newPasswordUrl + '/' + query.new_password_token;
+      } else if (query.verify_email_token) {
+          url = verifyEmailUrl + '/' + query.verify_email_token;
+      } else if (query.add_pdf_code) {
+          url = addPdfCodeUrl + '/' + query.add_pdf_code;
+      }
+      
+      iframe.setAttribute('class', 'linguahouse_profile_widget');
+      iframe.setAttribute('src', url);
+      iframe.setAttribute('width', width);
+      iframe.setAttribute('height', height);
+      iframe.setAttribute('frameborder', '0');
+      linguahouseContainer.current.appendChild(iframe);
     }
 
     return () => {
-      elem?.remove();
+      iframe?.remove();
     }
   }, [linguahouseContainer])
 
